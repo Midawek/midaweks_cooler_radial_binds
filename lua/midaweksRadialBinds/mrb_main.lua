e45ff547e8c07ae56a61d5b746a6edd1eb25cc43
Midawek <midawek@pm.me>
2026-07-30 23:37:28 +0200

    added fonts

--- lua/midaweksRadialBinds/mrb_main.lua
+++ lua/midaweksRadialBinds/mrb_main.lua
@@ -1,10 +1,12 @@
 -- MIDAWEK'S COOLER RADIAL BINDS
 
 MCRB = MCRB or {}
-MCRB.Vars = MCRB.Vars or {}
-
+MCRB.vars = MCRB.vars or {}
+MCRB.fonts = {
+    main = "Chakra Petch"
+}
 
 if CLIENT then
-    MCRB.Vars.Styles = CreateClientConVar("mcrb_cl_radial_style", 0, true, false,
+    MCRB.vars.Styles = CreateClientConVar("mcrb_cl_radial_style", 0, true, false,
         "Change the style of how radial binds looks, refer to documentation for information", 0)
 end
