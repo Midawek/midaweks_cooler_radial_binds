f54c86e386d3abd15cbca0dd2e226cfc18b61b73
Midawek <midawek@pm.me>
2026-07-31 01:16:33 +0200

    binds

--- lua/midaweksRadialBinds/mrb_main.lua
+++ lua/midaweksRadialBinds/mrb_main.lua
@@ -1,3 +1,16 @@
 -- MIDAWEK'S COOLER RADIAL BINDS
 
 MCRB = MCRB or {}
+
+MCRB.Binds = {
+    "cmd", "bind", "toggle", "instant", "fallngcmd", "burst", "dualedge"
+    --[[
+    cmd - when selected, pressing the bind key will activate this one.
+    bind - when selected, pressing the bind key will use it with + and -
+    toggle - when selected, pressing the bind key will toggle + and -
+    instant - when selected, this command will trigger instantly. It will then act like CMD.
+    falling_cmd - command triggers on falling edge
+    burst - command triggers for X seconds
+    dualedge - command triggers on rising and falling edge
+    ]]
+}
