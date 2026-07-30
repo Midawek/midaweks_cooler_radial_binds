78585741cf268555d9f37712ffb9608638429f7f
Midawek <midawek@pm.me>
2026-07-31 01:00:31 +0200

    remove mrb_main shit

--- lua/midaweksRadialBinds/mrb_main.lua
+++ lua/midaweksRadialBinds/mrb_main.lua
@@ -1,12 +1,3 @@
 -- MIDAWEK'S COOLER RADIAL BINDS
 
 MCRB = MCRB or {}
-MCRB.vars = MCRB.vars or {}
-MCRB.fonts = {
-    main = "Chakra Petch"
-}
-
-if CLIENT then
-    MCRB.vars.Styles = CreateClientConVar("mcrb_cl_radial_style", 0, true, false,
-        "Change the style of how radial binds looks, refer to documentation for information", 0)
-end
